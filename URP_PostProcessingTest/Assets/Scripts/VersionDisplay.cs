da93c7855661b4b008ec3ce445fe3ee253c65492
yuki4080 <yuki4080@users.noreply.github.com>
2019-12-27 16:53:18 +0900

    Preparation for Cloud Build

---
+++ URP_PostProcessingTest/Assets/Scripts/VersionDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VersionDisplay : MonoBehaviour
+{
+    public string _ColorSpace;
+    [SerializeField] TMP_Text msg;
+
+    void Update()
+    {
+        msg.text = Application.platform + ", Version Number: " + Application.unityVersion + ", " + SystemInfo.graphicsDeviceVersion + ", Color Space: " + _ColorSpace;
+    }
+}
